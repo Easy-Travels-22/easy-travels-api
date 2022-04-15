4dbb3b58b2585bfefe9325cbf908a7df2b77f6f1
Tan Eu Zin <taneuzin@u.nus.edu>
2022-04-15 14:38:31 +0800

    Renames TripRoutes to tripRoutes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
-const tripRouter = require("./routes/TripRoutes");
+const tripRouter = require("./routes/tripRoutes");
 
 app.use("/api/v1/trips", tripRouter);
 
