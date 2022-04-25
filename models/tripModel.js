1152c663515f41a023ef7afe8291acd90c074ada
Tan Eu Zin <taneuzin@u.nus.edu>
2022-04-26 01:17:25 +0800

    Updates Trip Model

--- models/tripModel.js
+++ models/tripModel.js
@@ -8,19 +8,32 @@ const tripSchema = new mongoose.Schema({
     required: true,
     maxLength: [100, "A Trip name should not exceed 100 characters"],
   },
+  description: {
+    type: String,
+    maxLength: [500, "Description should not exceed 500 characters"],
+  },
   startDate: {
     type: Date,
   },
   endDate: {
     type: Date,
   },
-  description: {
-    type: String,
-    maxLength: [500, "Description should not exceed 500 characters"],
-  },
   schedule: {
-    type: [[activitySchema]],
+    type: [[String]],
   },
+  createdOn: {
+    type: Date,
+    required: true,
+  },
+  tripOwner: {
+    type: String,
+    required: true,
+  },
+});
+
+tripSchema.pre("save", function (next) {
+  this.createdOn = Date.now();
+  next();
 });
 
 const Trip = mongoose.model("Trip", tripSchema);
