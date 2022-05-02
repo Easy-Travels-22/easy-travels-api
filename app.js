ff96f72dd2c03f20eeb557b9ace3a86c4f32b5df
Tan Eu Zin <taneuzin@u.nus.edu>
2022-05-02 12:34:12 +0800

    Implements all routes and global error handling

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ app.use(express.json());
 
 const tripRouter = require("./routes/tripRoutes");
 const activityRouter = require("./routes/activityRoutes");
+const userRouter = require("./routes/userRoutes");
+const errorController = require("./controllers/errorController");
 
 app.use("/api/v1/trips", tripRouter);
 app.use("/api/v1/activities", activityRouter);
+app.use("/api/v1/users", userRouter);
+
+app.use(errorController);
 
 module.exports = app;
