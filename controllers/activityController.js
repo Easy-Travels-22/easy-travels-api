f01c8320cccdf4c03985a325540b42b94175eb85
Tan Eu Zin <taneuzin@u.nus.edu>
2022-05-02 12:30:46 +0800

    Implements catchAsync for activity and tripController

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,7 +1,8 @@
 const Activity = require("../models/activityModel");
+const catchAsync = require("../utils/catchAsync");
 
 // Only for developers
-exports.getAllActivities = async (req, res) => {
+exports.getAllActivities = catchAsync(async (req, res, next) => {
   const activities = await Activity.find();
 
   res.status(200).json({
@@ -10,9 +11,9 @@ exports.getAllActivities = async (req, res) => {
       activities,
     },
   });
-};
+});
 
-exports.getActivity = async (req, res) => {
+exports.getActivity = catchAsync(async (req, res, next) => {
   const activity = await Activity.findById(req.params.id);
 
   res.status(200).json({
@@ -21,58 +22,37 @@ exports.getActivity = async (req, res) => {
       activity,
     },
   });
-};
+});
 
-exports.createActivity = async (req, res) => {
-  try {
-    const newActivity = await Activity.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: {
-        activity: newActivity,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "Failed to Create Activity",
-    });
-  }
-};
+exports.createActivity = catchAsync(async (req, res, next) => {
+  const newActivity = await Activity.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      activity: newActivity,
+    },
+  });
+});
 
-exports.updateActivity = async (req, res) => {
-  try {
-    const activity = await Activity.findByIdAndUpdate(req.params.id, req.body, {
-      runValidators: true,
-      new: true,
-    });
+exports.updateActivity = catchAsync(async (req, res, next) => {
+  const activity = await Activity.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.status(400).json({
+    status: "Successfully Updated",
+    data: {
+      activity,
+    },
+  });
+});
 
-    res.status(400).json({
-      status: "Successfully Updated",
-      data: {
-        activity,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "Failed to update Trip",
-      data: error,
-    });
-  }
-};
-
-exports.deleteActivity = async (req, res) => {
-  try {
-    const activity = await Activity.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      status: "success",
-      data: {
-        activity,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "Failed to delete Trip",
-      data: error,
-    });
-  }
-};
+exports.deleteActivity = catchAsync(async (req, res, next) => {
+  const activity = await Activity.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      activity,
+    },
+  });
+});
--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,109 +1,155 @@
-const { findById, findByIdAndDelete } = require("../models/tripModel");
+const Activity = require("../models/activityModel");
 const Trip = require("../models/tripModel");
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
-exports.getAllTrips = async (req, res) => {
-  try {
-    const trips = await Trip.find();
+exports.getAllTrips = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.body.requesterId);
+  const promises = [];
 
-    res.status(200).json({
-      status: "success",
-      data: trips,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-    });
+  for (let tripId of user.trips) {
+    promises.push(Trip.findById(tripId));
   }
-};
+  const trips = await Promise.all(promises);
 
-exports.createTrip = async (req, res) => {
-  try {
-    const newTrip = await Trip.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: {
-        trip: newTrip,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-    });
+  res.status(200).json({
+    status: "success",
+    data: trips,
+  });
+});
+
+exports.getTrip = catchAsync(async (req, res, next) => {
+  const trip = await Trip.findById(req.params.id);
+
+  if (trip.tripOwner !== req.body.requesterId) {
+    next(new AppError("Trip does not belong to logged in user"));
+    return;
   }
-};
+  res.status(200).json({
+    status: "success",
+    data: trip,
+  });
+});
 
-exports.updateTrip = async (req, res) => {
-  try {
-    const trip = await Trip.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.createTrip = catchAsync(async (req, res, next) => {
+  req.body.tripOwner = req.body.requesterId;
+  const newTrip = await Trip.create(req.body);
+  const user = await User.findById(req.body.tripOwner);
 
-    res.status(400).json({
-      status: "Successfully Updated",
-      data: {
-        trip,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "Failed to update Trip",
-      data: error,
-    });
+  user.trips.push(newTrip._id);
+  user.save();
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      trip: newTrip,
+    },
+  });
+});
+
+exports.updateTrip = catchAsync(async (req, res, next) => {
+  let trip = await Trip.findById(req.params.id);
+
+  if (!trip) {
+    next(new AppError("Trip not found"));
+    return;
+  } else if (trip.tripOwner !== req.body.requesterId) {
+    next(new AppError("Trip does not belong to logged in user"));
+    return;
   }
-};
 
-exports.deleteTrip = async (req, res) => {
-  try {
-    const trip = await Trip.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      status: "success",
-      data: {
-        trip,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "Failed to delete Trip",
-      data: error,
-    });
+  if (req.body.startDate || req.body.endDate) {
+    const newStartDate = req.body.startDate || trip.startDate;
+    const newEndDate = req.body.endDate || trip.endDate;
+    if (newEndDate <= newStartDate) {
+      next(new AppError("End Date must be after Start Date"));
+      return;
+    }
   }
-};
 
-exports.getSchedule = async (req, res) => {
-  try {
-    const trip = await Trip.find(req.params);
-    const schedule = trip ? trip.schedule : null;
-    res.status(200).json({
-      status: "success",
-      data: schedule,
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "Unable to get schedule",
-      data: error,
-    });
+  trip = await Trip.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: false,
+  });
+
+  res.status(200).json({
+    status: "Successfully Updated",
+    data: {
+      trip,
+    },
+  });
+});
+
+exports.deleteTrip = catchAsync(async (req, res, next) => {
+  let trip = await Trip.findById(req.params.id);
+
+  if (!trip) {
+    next(new AppError("Trip not found"));
+    return;
+  } else if (trip.tripOwner !== req.body.requesterId) {
+    next(new AppError("Trip does not belong to logged in user"));
+    return;
   }
-};
 
-exports.updateSchedule = async (req, res) => {
-  try {
-    const scheduleUpdate = { schedule: req.body.schedule };
-    const trip = await Trip.findByIdAndUpdate(req.params.id, scheduleUpdate, {
-      new: true,
-      runValidators: true,
-    });
+  trip = await Trip.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    status: "success",
+  });
+});
 
-    res.status(200).json({
-      status: "Updated Schedule",
-      data: {
-        trip,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "Unable to update schedule",
-      data: error,
-    });
+exports.getSchedule = catchAsync(async (req, res, next) => {
+  const trip = await Trip.findById(req.params.id);
+
+  if (!trip) {
+    next(new AppError("Trip not found"));
+    return;
+  } else if (trip.tripOwner !== req.body.requesterId) {
+    next(new AppError("Trip does not belong to logged in user"));
+    return;
   }
-};
+
+  const scheduleById = trip.schedule;
+  const promises = [];
+
+  for (let activityId of scheduleById) {
+    promises.push(Activity.findById(activityId));
+  }
+
+  const activities = await Promise.all(promises);
+
+  res.status(200).json({
+    status: "success",
+    activities,
+  });
+});
+
+exports.updateSchedule = catchAsync(async (req, res, next) => {
+  let trip = await Trip.findById(req.params.id);
+
+  if (!trip) {
+    next(new AppError("Trip not found"));
+    return;
+  } else if (trip.tripOwner !== req.body.requesterId) {
+    next(new AppError("Trip does not belong to logged in user"));
+    return;
+  }
+  const scheduleUpdate = { scheduleById: req.body.scheduleById };
+  trip = await Trip.findByIdAndUpdate(req.params.id, scheduleUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "Updated Schedule",
+    data: {
+      trip,
+    },
+  });
+});
+
+exports.assignTripOwner = catchAsync(async (req, res, next) => {
+  const decoded = await promisify(jwt.decode)(token, process.env.JWT_SECRET);
+  req.body.tripOwner = decoded.id;
+});
