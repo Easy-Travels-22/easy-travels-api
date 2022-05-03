14b0c5fa68d56aa27ce72eb3ebaa7d63d2d75cfc
Tan Eu Zin <taneuzin@u.nus.edu>
2022-05-03 11:17:57 +0800

    Implements rate-limiter and data-sanitization

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const app = express();
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP",
+});
 
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+
+// DATA SANITIZATION against NoSQL Query Injection
+app.use(mongoSanitize());
+
+// DATA SANITIZATION against XSS
+app.use(xss());
+
+app.use(helmet());
+
+app.use("/api", limiter);
 
 const tripRouter = require("./routes/tripRoutes");
 const activityRouter = require("./routes/activityRoutes");
