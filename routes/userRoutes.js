1b05d13601ba6bdd88090780109e1f7587237f72
Tan Eu Zin <taneuzin@u.nus.edu>
2022-05-03 11:22:58 +0800

    Changes route names to updateMe and deleteMe

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,30 @@
 const express = require("express");
 
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  updatePassword,
+} = require("../controllers/authController");
 
 const {
   getUserTrips,
   getAllUsers,
   getUser,
-  updateUser,
+  updateMe,
   deleteUser,
 } = require("../controllers/userController");
+const { protect, restrictTo } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/sign-up", registerUser);
 router.post("/login", loginUser);
+router.patch("/updateMyPassword", protect, updatePassword);
+router.patch("/updateMe", protect, updateMe);
+
 router.get("/get-user-trips", getUserTrips);
 
-router.route("/").get(getAllUsers);
-router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+router.route("/").get(protect, restrictTo("Admin"), getAllUsers);
+router.route("/:id").get(getUser);
 
 module.exports = router;
