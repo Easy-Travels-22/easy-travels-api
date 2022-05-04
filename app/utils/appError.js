2ca25c4b87e3b7434074d409b0a7e76dd3cd4b0e
Tan Eu Zin <taneuzin@u.nus.edu>
2022-05-04 23:17:07 +0800

    Updates AppError

--- app/utils/appError.js
+++ app/utils/appError.js
@@ -4,6 +4,7 @@ class AppError extends Error {
 
     this.statusCode = statusCode;
     this.isOperational = true;
+    this.message = message;
 
     Error.captureStackTrace(this, this.constructor);
   }
